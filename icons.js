bdefc8cd58fe711af14d7d5d00ac293ed68d6464
Micah Santos <micah72@uw.edu>
2025-03-23 23:22:18 -0700

    Delete icons.js

--- icons.js
+++
@@ -1,4 +0,0 @@
-export const flag = <i className="fa-regular fa-font-awesome"></i>;
-export const list = <i className="fa-regular fa-list"></i>;
-export const check = <i className="fa-regular fa-check"></i>;
-export const pen2square = <i className="fa-regular fa-pen-to-square"></i>;
